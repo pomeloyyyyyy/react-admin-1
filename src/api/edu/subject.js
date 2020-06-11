5fd89642d98eb2eef19e5018bccddd919089cc32
pomeloyyyyyy <664719559@qq.com>
2020-06-11 08:37:36 +0800

    0

---
+++ src/api/edu/subject.js
@@ -0,0 +1,35 @@
+import request from "@utils/request";
+
+// 模块请求公共前缀
+const BASE_URL = "/admin/edu/subject"; // 线上访问地址，内部会通过服务器代理代理真实线上地址
+
+// mock地址
+const MOCK_BASE_URL = `http://localhost:9527${BASE_URL}`;
+
+// 获取一级分类分页列表数据
+export function reqGetSubjectList(page, limit) {
+  return request({
+    url: `${MOCK_BASE_URL}/${page}/${limit}`,
+    method: "GET",
+  });
+}
+
+// 获取二级分类分页列表数据
+export function reqGetSubSubjectList(parentId) {
+  return request({
+    url: `${MOCK_BASE_URL}/get/${parentId}`,
+    method: "GET",
+  });
+}
+
+// 添加课程分类
+export function reqAddSubject(title, parentId) {
+  return request({
+    url: `${MOCK_BASE_URL}/save`,
+    method: "POST",
+    data: { // 请求参数
+      title,
+      parentId,
+    },
+  });
+}
--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -1,187 +1,145 @@
 import React, { Component } from "react";
-import { Table, Button, Tooltip } from "antd";
+// 引入antd组件
+import { Button, Table } from "antd";
+// 引入antd字体图标
 import { PlusOutlined, FormOutlined, DeleteOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
+
+import { getSubjectList, getSubSubjectList } from "./redux";
 
 import "./index.less";
 
-const subjects = [
+@connect(
+  (state) => ({ subjectList: state.subjectList }), // 状态数据
   {
-    id: "1178214681118568449",
-    title: "后端开发",
-    children: [
-      {
-        id: "1178214681139539969",
-        title: "Java",
-        children: [],
-      },
-      {
-        id: "1178585108407984130",
-        title: "Python",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681181483010",
-    title: "前端开发",
-    children: [
-      {
-        id: "1178214681210843137",
-        title: "JavaScript",
-        children: [],
-      },
-      {
-        id: "1178585108454121473",
-        title: "HTML/CSS",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681231814658",
-    title: "云计算",
-    children: [
-      {
-        id: "1178214681252786178",
-        title: "Docker",
-        children: [],
-      },
-      {
-        id: "1178214681294729217",
-        title: "Linux",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681324089345",
-    title: "系统/运维",
-    children: [
-      {
-        id: "1178214681353449473",
-        title: "Linux",
-        children: [],
-      },
-      {
-        id: "1178214681382809602",
-        title: "Windows",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681399586817",
-    title: "数据库",
-    children: [
-      {
-        id: "1178214681428946945",
-        title: "MySQL",
-        children: [],
-      },
-      {
-        id: "1178214681454112770",
-        title: "MongoDB",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681483472898",
-    title: "大数据",
-    children: [
-      {
-        id: "1178214681504444418",
-        title: "Hadoop",
-        children: [],
-      },
-      {
-        id: "1178214681529610242",
-        title: "Spark",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681554776066",
-    title: "人工智能",
-    children: [
-      {
-        id: "1178214681584136193",
-        title: "Python",
-        children: [],
-      },
-    ],
-  },
-  {
-    id: "1178214681613496321",
-    title: "编程语言",
-    children: [
-      {
-        id: "1178214681626079234",
-        title: "Java",
-        children: [],
-      },
-    ],
-  },
-].map((item) => {
-  if (!item.children || !item.children.length) {
-    return {
-      ...item,
-      children: null,
-    };
+    // 更新状态数据的方法
+    getSubjectList,
+    getSubSubjectList,
   }
-  return {
-    ...item,
-    children: item.children.map((item) => {
-      if (!item.children || !item.children.length) {
-        return {
-          ...item,
-          children: null,
-        };
-      }
-      return item;
-    }),
+)
+class Subject extends Component {
+  state = {
+    expandedRowKeys: [], // 展开项
   };
-});
 
-export default class Subject extends Component {
-  columns = [
-    {
-      title: "分类名称",
-      dataIndex: "title",
-    },
-    {
-      title: "操作",
-      fixed: "right",
-      width: 200,
-      render: (data) => {
-        return (
-          <div>
-            <Tooltip title="更新课程分类">
-              <Button type="primary" style={{ margin: "0 10px" }}>
-                <FormOutlined />
-              </Button>
-            </Tooltip>
-            <Tooltip title="删除课程分类">
-              <Button type="danger">
-                <DeleteOutlined />
-              </Button>
-            </Tooltip>
-          </div>
-        );
-      },
-    },
-  ];
+  componentDidMount() {
+    // 代表一上来请求第一页数据
+    this.props.getSubjectList(1, 10);
+  }
+
+  /*
+    问题1： 展开第一项 展开第二项 关闭第二项 第一项数据发生了更新
+      原因：
+        不管展开还是关闭都会触发 handleExpandedRowsChange
+        [1, 2] --> [1] 就会拿着 1 重新发送请求，所以更新了第一项数据
+      解决：
+        关键点：判断当前是 展开 还是 关闭~
+          如果是展开，就要发送请求更新数据，还要更新expandedRowKeys
+          如果是关闭，就只要更新expandedRowKeys，而不需要发送请求
+        展开，长度会+1，而关闭，长度会-1
+        当前expandedRowKeys --> this.state.expandedRowKeys
+        最新expandedRowKeys --> 函数传入的参数
+  */
+  // 点击展开一级菜单
+  // expandedRowKeys展开的行项（如果有没有展开的会自动去掉）
+  handleExpandedRowsChange = (expandedRowKeys) => {
+    console.log("handleExpandedRowsChange", expandedRowKeys);
+    // 长度
+    const length = expandedRowKeys.length;
+
+    // 如果最新长度大于之前的长度，说明就是展开~
+    if (length > this.state.expandedRowKeys.length) {
+      // 点击要展开的最后一项值~
+      const lastKey = expandedRowKeys[length - 1];
+      // 发送请求，请求要展开菜单的二级菜单数据
+      this.props.getSubSubjectList(lastKey);
+    }
+
+    // 更新state --> 告诉Table哪个子菜单需要展开
+    this.setState({
+      expandedRowKeys,
+    });
+  };
+
+  // 显示添加页面
+  showAddSubject = () => {
+    this.props.history.push("/edu/subject/add");
+  };
 
   render() {
+    const { subjectList, getSubjectList } = this.props;
+    const { expandedRowKeys } = this.state;
+
+    const columns = [
+      {
+        // 表头显示的内容
+        title: "分类名称",
+        // 当前列要显示data中哪个数据（显示数据的key属性）
+        // data[dataIndex]
+        dataIndex: "title",
+        // 遍历元素需要唯一key属性
+        key: "title",
+      },
+      {
+        title: "操作",
+        dataIndex: "",
+        key: "action",
+        // 列的宽度
+        width: 200,
+        // 默认情况下，渲染的内容是纯文本
+        // 如果想渲染成其他方案（按钮）需要用render方法指定
+        render: () => (
+          <>
+            <Button type="primary">
+              <FormOutlined />
+            </Button>
+            <Button type="danger" className="subject-btn">
+              <DeleteOutlined />
+            </Button>
+          </>
+        ),
+      },
+    ];
+
     return (
       <div className="subject">
-        <Button type="primary" style={{ marginBottom: 20, marginLeft: 20 }}>
+        <Button
+          type="primary"
+          className="subject-btn"
+          onClick={this.showAddSubject}
+        >
           <PlusOutlined />
           新建
         </Button>
-        <Table rowKey="id" columns={this.columns} dataSource={subjects} />
+        <Table
+          columns={columns} // 决定列头
+          expandable={{
+            // 内部默认会使用children作为展开的子菜单
+            // 也就是说，如果要展开的数据有children属性，才会有展开图标，就会作为子菜单展开~
+            // 负责展开行
+            // 展开哪些行？[行的key值, 行的key值...]
+            // [_id, _id]
+            expandedRowKeys,
+            // 展开行触发的方法。
+            // 将展开的行[1, 2, 3]作为参数传入
+            onExpandedRowsChange: this.handleExpandedRowsChange,
+          }}
+          dataSource={subjectList.items} // 决定每一行显示的数据
+          rowKey="_id" // 指定key属性的值是_id
+          pagination={{
+            // 分页器
+            total: subjectList.total, // 数据总数
+            showQuickJumper: true, // 是否显示快速跳转
+            showSizeChanger: true, // 是否显示修改每页显示数量
+            pageSizeOptions: ["5", "10", "15", "20"],
+            defaultPageSize: 10,
+            onChange: getSubjectList, // 页码发生变化触发的回调
+            onShowSizeChange: getSubjectList,
+          }}
+        />
       </div>
     );
   }
 }
+
+export default Subject;
